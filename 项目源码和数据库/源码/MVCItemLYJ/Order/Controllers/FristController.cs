b1b219bbef84add730a908efd8d853c5b2aacbfc
Single010 <1373284200@qq.com>
2020-06-22 00:58:40 +0800

    完成主页

--- 项目源码和数据库/源码/MVCItemLYJ/Order/Controllers/FristController.cs
+++ 项目源码和数据库/源码/MVCItemLYJ/Order/Controllers/FristController.cs
@@ -19,14 +19,22 @@ namespace Order.Controllers
         public ActionResult Main()
         {
             List<Dept> dept = db.Dept.ToList();
+            List<Hospital> hos = db.Hospital.ToList();
             ViewBag.dept = dept;
+            ViewBag.hos = hos;
             return View();
         }
 
         public ActionResult FindMed(int? id)
         {
-            List<Mediciner> findmed = db.Mediciner.Where(p => p.Did == id).ToList();
+            List<Mediciner> findmed = db.Mediciner.Where(p => p.Did == id).Take(5).ToList();
             return PartialView(findmed);
+        }
+
+        public ActionResult FindHos(string name)
+        {
+            List<Hospital> findhos = db.Hospital.Where(p => p.Address == name).Take(3).ToList();
+            return PartialView(findhos);
         }
 
     }
